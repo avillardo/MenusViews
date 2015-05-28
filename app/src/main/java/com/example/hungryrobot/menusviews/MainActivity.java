2702e26f521f939f795aaf13e9cf8e0834f957d3
aldene villardo <aldenevillardo@gmail.com>
2015-05-28 18:46:13 -0400

    removed ActionBar (deprecated) then added Toolbar.

--- app/src/main/java/com/example/hungryrobot/menusviews/MainActivity.java
+++ app/src/main/java/com/example/hungryrobot/menusviews/MainActivity.java
@@ -2,16 +2,23 @@ package com.example.hungryrobot.menusviews;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar= (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 
     @Override
@@ -30,6 +37,7 @@ public class MainActivity extends ActionBarActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Toast.makeText(this,"Hello "+item.getTitle(),Toast.LENGTH_SHORT).show();
             return true;
         }
 
